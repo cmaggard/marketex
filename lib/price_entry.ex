968ca22789c3ba3128ea6f55ca7ed1730f296aaa
Cody Maggard <codym@everfi.com>
2016-10-01 01:28:17 -0400

    Dialyxir and type specs for PriceEntry

--- lib/price_entry.ex
+++ lib/price_entry.ex
@@ -1,14 +1,24 @@
 defmodule Marketex.PriceEntry do
+  @type order_placer :: atom
+  @type quantity :: integer
+
+  @type order :: {order_placer, quantity}
+  @type price_entry :: :queue.queue(order)
+
+  @spec new :: price_entry
   def new, do: :queue.new()
 
+  @spec count(price_entry) :: integer
   def count(entry) do
     :queue.len entry
   end
 
-  def add(entry, item) do
-    :queue.in item, entry
+  @spec add(price_entry, order) :: price_entry
+  def add(entry, order) do
+    :queue.in order, entry
   end
 
+  @spec front_quantity(price_entry) :: integer
   def front_quantity(entry) do
     case :queue.peek(entry) do
       :empty -> 0
